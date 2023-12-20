e077255288213e9deb2e8d4488404bd6da1b7292
Tarasova Ekaterina <eyakimova@solvo.ru>
2023-12-20 09:54:27 +0300

    add project

---
+++ src/main/java/com/zoomeduz/tictactoe/TicTacToe.java
@@ -0,0 +1,12 @@
+package com.zoomeduz.tictactoe;
+
+/*
+ * Tic Tac Toe game
+ *
+ * @author zoomeduz
+ */
+public class TicTacToe {
+    public static void main(String[] args){
+        System.out.println("Это начало!");
+    }
+}
