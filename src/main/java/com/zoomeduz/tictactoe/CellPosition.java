f06661b070bc78c7981ed8304b30f07ba22d2cbe
Zoomeduz <zoomeduz@gmail.com>
2024-02-17 18:48:29 +0300

    modified CellPosition class

--- src/main/java/com/zoomeduz/tictactoe/CellPosition.java
+++ src/main/java/com/zoomeduz/tictactoe/CellPosition.java
@@ -7,6 +7,11 @@ package com.zoomeduz.tictactoe;
 class CellPosition {
     int column;
     int row;
+    
+    CellPosition() {
+        this.row = 0;
+        this.column = 0;
+    }
 
     CellPosition(int rowNumber, int columnNumber) {
         this.row = rowNumber;
@@ -18,5 +23,14 @@ class CellPosition {
         int r = row + 1;
         int c = column + 1;
         return (r - 1) * numberOfColumns + c;
-    } 
+    }
+    
+    boolean equals(CellPosition pos) {
+        if (pos.row == this.row && pos.column == this.column) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
